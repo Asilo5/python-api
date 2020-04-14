89cc01fa2ef7ba833cb9809ab84b9d66ce2be073
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-13 21:25:42 -0600

    Add python and docker code to start db

--- device_registry/__init__.py
+++ device_registry/__init__.py
@@ -1,9 +1,36 @@
+import markdown
+import os
+import shelve
+
 # Import the framework
-from flask import Flask
+from flask import Flask, g
 
 # Create an instance of Flask
 app = Flask(__name__)
 
+import sqlite3
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = shelve.open('devices.db')
+    return db
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
 @app.route('/')
-def hello():
-    
+def index():
+    """ Present some document"""
+    # Open the README file
+    with open(os.path.dirname(app.root_path) + '/README.md', 'r') as markdown_file:
+        
+        # Read the content of the file
+        content = markdown_file.read()
+
+        # Convert to HTML
+        return markdown.markdown(content)
--- run.py
+++ run.py
@@ -1,2 +1,4 @@
-print('Hello, World!')
+from device_registry import app
+
+app.run(host='0.0.0.0', port=80, debug=True)
 
